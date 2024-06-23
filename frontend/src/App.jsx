70d5b7b240a4dcf93f6874d7bcc057fd3dd5aae0
Cenote <himangshukalita131@gmail.com>
2024-06-24 02:15:32 +0530

    Set up routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
-  return <div className="text-red-500">App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
